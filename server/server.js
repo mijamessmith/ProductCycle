89c20ba18cd757638ef692862a87dc5e5f4b1395
Michael Smith <mijamessmith@gmail.com>
2021-04-15 15:06:27 -0700

    inserted video for README

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get('/products',(req, res) => {
     res.send(result.data);
   })
   .catch((error) => {
-    console.log(error);
+    res.send(404).send(error);
   })
 });
 
@@ -56,7 +56,7 @@ app.get('/products/:id', (req,res) => {
     res.send(result.data);
   })
   .catch((error) => {
-    console.log(error);
+    res.status(404).send(error);
   })
 })
 
